ae0d1a19c702db01c138b2910f5aa412c6859f0b
Scott Patterson <scott.neil.patterson@gmail.com>
2024-12-04 14:09:15 +1300

    Remove lefthand options and rename

--- reactjs-gen-ai-apps/src/Layout.jsx
+++ reactjs-gen-ai-apps/src/Layout.jsx
@@ -24,38 +24,19 @@ export default () => {
                 <SideNavigation
                     activeHref={activeHref}
 
-                    header={
-                        { href: "/", text: "Demos" }
-                    }
+                    // header={
+                    //     { href: "/", text: "Chatbots" }
+                    // }
                     items={
                         [
-                            {
-                                type: "section", text: "Chat With Amazon Bedrock Multimodal", items: [
-/*                                     { type: "link", href: "/llm", text: "Chat Q&A" },
-                                    { type: "link", href: "/chat", text: "Chat with Memory" }, */
-                                    { type: "link", href: "/multimodal", text: "Multimodal Chatbot" },
-
-                                ]
-                            },
-                            {
-                                type: "section", text: "System Prompts", items: [
-                                    { type: "link", href: "/prompt", text: "Prompts" }
-                                ]
-                            },
-                            { type: "divider" },
 
                             {
-                                type: "section", text: "Knowledge Bases for Amazon Bedrock", items: [
-                                    { type: 'link', text: `Amazon Bedrock Retrieve => LLM`, href: `/retrieve` },
-                                    { type: 'link', text: `Amazon Bedrock Retrieve & Generate`, href: `/retrieveandgenerate` }
+                                type: "section", text: "Knowledge Bases using Amazon Bedrock", items: [
+                                    // { type: 'link', text: `Amazon Bedrock Retrieve => LLM`, href: `/retrieve` },
+                                    { type: 'link', text: `Digital Services Consultation`, href: `/retrieveandgenerate` }
                                 ]
                             },
-                            { type: "divider" },
-                            {
-                                type: "section", text: "Agents for Amazon Bedrock", items: [
-                                { type: 'link', text: `Agents`, href: `/bedrockagent` },
-                            ]
-                        }
+                            { type: "divider" }
                         ]
                     }
                     onFollow={event => {
